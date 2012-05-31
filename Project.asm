b9ae9e6ef2d173cbc8041ca523de01b02830b985
Lisa <lisaleemcb@gmail.com>
2012-05-31 14:27:32 -0700

    second draft

--- Project.asm
+++ Project.asm
@@ -1,34 +1,73 @@
-; I don't know how to do this
+; Lisa McBride & Jared Kofron
+; PHYS 335 - PIC radio project
+; v0.1
 
 ;includes and such
 	processor 16f84
 	include <p16f84.inc>
 	__config _RC_OSC & _WDT_OFF & _PWRTE_ON
 
-	EQU M 0x01
+	EQU M 0x01 		; FIX MY ADDRESSES
 	EQU PHASEACC 0x02
+	EQU TEMP		; TEMP VARIABLE, FIX ME
 	
 ; program body
 
 	org 0
 
 	goto initialize
-
 loop	movf M,0
 	addwf PHASEACC,1
+	
+	btfsc PHASEACC,7
+	call MSB7Set
 
-	goto convert
-
-	movwf PORTB
-
+	btfss PHASEACC,7
+	call MSB7notSet
 	goto loop
 
 initialize:			; prepares PORTB for output, PORTA for input
 
 	return
 
-convert:			; creates sine wave
-	; assumes puts value into to the W reg
+MSB7Set:
+	btfsc PHASEACC,6
+	goto MSB6Set
+	movlf 0x3F
+	andwf PHASEACC,0
+	sublw 0x100
+	call lut
+	movwf PORTB
+	goto end
 
+MSB6Set
+	movlf 0x3F
+	movwf TEMP
+	andwf PHASEACC,0
+	subwf TEMP,0
+	sublw 0x100
+	call lut
+	movwf PORTB
+	goto end
+end	noop
 	return
 
+MSB7notSet:
+	btfsc PHASEACC,6
+	goto MSB6Set
+	movlf 0x3F
+	andwf PHASEACC,0
+	call lut
+	movwf PORTB
+	goto end
+
+MSB6Set
+	movlw 0x3F
+	movwf TEMP
+	andwf PHASEACC,0
+	subwf TEMP,0
+	call lut
+	movwf PORTB
+	goto end
+end	nop
+	return
